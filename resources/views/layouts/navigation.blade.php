0a7da001f78e4cc047f045cdfaec5070d3a582b0
jc sarmiento <jacinto011200@gmail.com>
2026-02-09 22:24:40 +0800

    add all the following data of rthe navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -58,71 +58,6 @@
 										</div>
 									</a>
 								</li>
-								<li class="notification-message">
-									<a href="activities.html">
-										<div class="media">
-											<span class="avatar">
-												<img alt="" src="assets/img/profiles/avatar-03.jpg">
-											</span>
-											<div class="media-body">
-												<p class="noti-details"><span class="noti-title">Tarah Shropshire</span>
-													changed the task name <span class="noti-title">Appointment booking
-														with payment gateway</span></p>
-												<p class="noti-time"><span class="notification-time">6 mins ago</span>
-												</p>
-											</div>
-										</div>
-									</a>
-								</li>
-								<li class="notification-message">
-									<a href="activities.html">
-										<div class="media">
-											<span class="avatar">
-												<img alt="" src="assets/img/profiles/avatar-06.jpg">
-											</span>
-											<div class="media-body">
-												<p class="noti-details"><span class="noti-title">Misty Tison</span>
-													added <span class="noti-title">Domenic Houston</span> and <span
-														class="noti-title">Claire Mapes</span> to project <span
-														class="noti-title">Doctor available module</span></p>
-												<p class="noti-time"><span class="notification-time">8 mins ago</span>
-												</p>
-											</div>
-										</div>
-									</a>
-								</li>
-								<li class="notification-message">
-									<a href="activities.html">
-										<div class="media">
-											<span class="avatar">
-												<img alt="" src="assets/img/profiles/avatar-17.jpg">
-											</span>
-											<div class="media-body">
-												<p class="noti-details"><span class="noti-title">Rolland Webber</span>
-													completed task <span class="noti-title">Patient and Doctor video
-														conferencing</span></p>
-												<p class="noti-time"><span class="notification-time">12 mins ago</span>
-												</p>
-											</div>
-										</div>
-									</a>
-								</li>
-								<li class="notification-message">
-									<a href="activities.html">
-										<div class="media">
-											<span class="avatar">
-												<img alt="" src="assets/img/profiles/avatar-13.jpg">
-											</span>
-											<div class="media-body">
-												<p class="noti-details"><span class="noti-title">Bernardo Galaviz</span>
-													added new task <span class="noti-title">Private chat module</span>
-												</p>
-												<p class="noti-time"><span class="notification-time">2 days ago</span>
-												</p>
-											</div>
-										</div>
-									</a>
-								</li>
 							</ul>
 						</div>
 						<div class="topnav-dropdown-footer">
@@ -162,78 +97,6 @@
 										</div>
 									</a>
 								</li>
-								<li class="notification-message">
-									<a href="chat.html">
-										<div class="list-item">
-											<div class="list-left">
-												<span class="avatar">
-													<img alt="" src="assets/img/profiles/avatar-02.jpg">
-												</span>
-											</div>
-											<div class="list-body">
-												<span class="message-author">John Doe</span>
-												<span class="message-time">6 Mar</span>
-												<div class="clearfix"></div>
-												<span class="message-content">Lorem ipsum dolor sit amet, consectetur
-													adipiscing</span>
-											</div>
-										</div>
-									</a>
-								</li>
-								<li class="notification-message">
-									<a href="chat.html">
-										<div class="list-item">
-											<div class="list-left">
-												<span class="avatar">
-													<img alt="" src="assets/img/profiles/avatar-03.jpg">
-												</span>
-											</div>
-											<div class="list-body">
-												<span class="message-author"> Tarah Shropshire </span>
-												<span class="message-time">5 Mar</span>
-												<div class="clearfix"></div>
-												<span class="message-content">Lorem ipsum dolor sit amet, consectetur
-													adipiscing</span>
-											</div>
-										</div>
-									</a>
-								</li>
-								<li class="notification-message">
-									<a href="chat.html">
-										<div class="list-item">
-											<div class="list-left">
-												<span class="avatar">
-													<img alt="" src="assets/img/profiles/avatar-05.jpg">
-												</span>
-											</div>
-											<div class="list-body">
-												<span class="message-author">Mike Litorus</span>
-												<span class="message-time">3 Mar</span>
-												<div class="clearfix"></div>
-												<span class="message-content">Lorem ipsum dolor sit amet, consectetur
-													adipiscing</span>
-											</div>
-										</div>
-									</a>
-								</li>
-								<li class="notification-message">
-									<a href="chat.html">
-										<div class="list-item">
-											<div class="list-left">
-												<span class="avatar">
-													<img alt="" src="assets/img/profiles/avatar-08.jpg">
-												</span>
-											</div>
-											<div class="list-body">
-												<span class="message-author"> Catherine Manseau </span>
-												<span class="message-time">27 Feb</span>
-												<div class="clearfix"></div>
-												<span class="message-content">Lorem ipsum dolor sit amet, consectetur
-													adipiscing</span>
-											</div>
-										</div>
-									</a>
-								</li>
 							</ul>
 						</div>
 						<div class="topnav-dropdown-footer">
@@ -245,8 +108,9 @@
 
 				<li class="nav-item dropdown has-arrow main-drop">
 					<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-						<span class="user-img"><img src="assets/img/profiles/avatar-21.jpg" alt="default-profile">
-							<span class="status online"></span></span>
+						<div class="user-img"><img src="assets/img/profiles/avatar-21.jpg" alt="default-profile">
+							<span class="status online"></span>
+                        </div>
 						<span>Admin</span>
 					</a>
 					<div class="dropdown-menu">
